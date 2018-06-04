a87dfce72f5c43a859b87689dd98155429bd5dd4
Blaze Hadzik <hadzikblazej@gmail.com>
2018-06-04 15:56:59 +1000

    Add redux-thunk

--- src/store.js
+++ src/store.js
@@ -1,11 +1,12 @@
 import rootReducer from "./reducers";
 import { createStore, applyMiddleware } from "redux";
 import { routerMiddleware } from "react-router-redux";
+import thunk from "redux-thunk";
 
 import logger from "redux-logger";
 
 export const getStore = history => {
-  const middlewares = [routerMiddleware(history), logger];
+  const middlewares = [routerMiddleware(history), thunk, logger];
 
   return createStore(rootReducer, applyMiddleware(...middlewares));
 };
