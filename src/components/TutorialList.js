03d3f947676f8adb77350a77fcc02f33bdea7908
Blaze Hadzik <hadzikblazej@gmail.com>
2018-06-06 13:16:45 +1000

    Update TutorialList to render 1 section only.

--- src/components/TutorialList.js
+++ src/components/TutorialList.js
@@ -3,14 +3,13 @@ import React from "react";
 import TutorialItem from "./TutorialItem";
 
 const TutorialList = ({ list }) => (
-  <div className="columns">
-    <div className="column is-three-quarters">
+  <section className="section">
+    <div className="container">
       {list.map((tutorial, index) => (
         <TutorialItem key={index} {...tutorial} />
       ))}
     </div>
-    <div className="column">You might also be interested in:</div>
-  </div>
+  </section>
 );
 
 export default TutorialList;
