772bc747531f7cd51d3fb70c7f0a1c4c02f66715
Blazej Hadzik <hadzikblazej@gmail.com>
2018-06-12 22:33:27 +1000

    Move topicsFilter reducer to generic filter reducer.

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -1,5 +1,26 @@
 import { combineReducers } from "redux";
-import topicsFilter from "./topicsFilter";
+import * as types from "../constants/ActionTypes";
+import { LOCATION_CHANGE } from "react-router-redux";
+
+const defaultFilter = {
+  name: {
+    value: ""
+  },
+  category: {
+    value: ""
+  }
+};
+
+const topicsFilter = (state = defaultFilter, action) => {
+  switch (action.type) {
+    case LOCATION_CHANGE:
+      return defaultFilter;
+    case types.FILTER_TOPICS:
+      return { ...state, ...action.activeFilters };
+    default:
+      return state;
+  }
+};
 
 export default combineReducers({
   topicsFilter
