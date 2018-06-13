830b1bf9405a9aec8fe73558331ed0dc3d1790b0
Blaze Hadzik <hadzikblazej@gmail.com>
2018-06-13 14:35:18 +1000

    App component to use new HomePage component. Remove unused component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { Route } from "react-router";
 import Page from "./Page";
-import HomePage from "../containers/HomePage";
+import HomePage from "./HomePage";
 import TopicPage from "../containers/TopicPage";
-import RedirectPage from "../components/RedirectPage";
 
 const App = () => (
   <Page>
     <Route exact path="/" component={HomePage} />
     <Route path="/topics/:slug" component={TopicPage} />
-    <Route path="/goto/:url" component={RedirectPage} />
   </Page>
 );
 
