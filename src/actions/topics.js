01b04b43a389c8dbedee5bc69d1b576593d3ba4e
Blaze Hadzik <hadzikblazej@gmail.com>
2018-06-13 14:39:16 +1000

    Add topics actions.

--- src/actions/topics.js
+++ src/actions/topics.js
@@ -1,29 +1,39 @@
 import api from "../api";
 import * as types from "../constants/ActionTypes";
 
-export const followTopic = id => {
-  return {
-    type: types.FOLLOW_TOPIC,
-    id
-  };
+const shouldFetchTopics = ({ topics }) => {
+  const topicItems = topics.entities;
+
+  if (!topicItems || topicItems.length === 0) {
+    return true;
+  } else if (topics.status.isFetching) {
+    return false;
+  } else {
+    return topics.status.didInvalidate;
+  }
 };
 
-export const unfollowTopic = id => {
-  return {
-    type: types.UNFOLLOW_TOPIC,
-    id
-  };
-};
-
-export const getTopics = () => dispatch => {
+const fetchTopics = () => dispatch => {
   dispatch({
-    type: types.TOPICS_REQUESTED
+    type: types.FETCH_TOPICS_REQUESTED
   });
 
-  api.getTopics().then(data => {
-    dispatch({
-      type: types.FETCH_TOPICS_SUCCESS,
-      topics: data
-    });
-  });
+  api
+    .getTopics()
+    .then(data => {
+      dispatch({
+        type: types.FETCH_TOPICS_SUCCESS,
+        topics: data
+      });
+    })
+    .catch(() => dispatch({ type: types.FETCH_TOPICS_ERROR }));
+};
+
+export const invalidateTopics = () => dispatch =>
+  dispatch({ type: types.INVALIDATE_TOPICS });
+
+export const fetchTopicsIfNeeded = () => (dispatch, getState) => {
+  if (shouldFetchTopics(getState())) {
+    return dispatch(fetchTopics());
+  }
 };
