62f17ea490142e2f389b04e51d19beea0420abbd
Blaze Hadzik <hadzikblazej@gmail.com>
2018-06-13 14:39:08 +1000

    Add tutorials actions.

--- src/actions/tutorials.js
+++ src/actions/tutorials.js
@@ -1,15 +1,46 @@
 import api from "../api";
 import * as types from "../constants/ActionTypes";
 
-export const getTutorials = slug => dispatch => {
+const fetchTopics = slug => dispatch => {
   dispatch({
-    type: types.TUTORIALS_REQUESTED
+    type: types.FETCH_TUTORIALS_REQUESTED,
+    slug
   });
 
-  api.getTutorials(slug).then(data => {
-    dispatch({
-      type: types.FETCH_TUTORIALS_SUCCESS,
-      tutorials: data
-    });
-  });
+  api
+    .getTutorials(slug)
+    .then(data => {
+      dispatch({
+        type: types.FETCH_TUTORIALS_SUCCESS,
+        tutorials: data,
+        slug
+      });
+    })
+    .catch(() =>
+      dispatch({
+        type: types.FETCH_TUTORIALS_ERROR,
+        slug
+      })
+    );
+};
+
+const shouldFetchTutorials = (state, slug) => {
+  const tutorials = state.tutorials[slug];
+
+  if (!tutorials) {
+    return true;
+  } else if (tutorials.isFetching) {
+    return false;
+  } else {
+    return tutorials.didInvalidate;
+  }
+};
+
+export const invalidateTutorials = slug => dispatch =>
+  dispatch({ type: types.INVALIDATE_TUTORIALS, slug });
+
+export const fetchTutorialsIfNeeded = slug => (dispatch, getState) => {
+  if (shouldFetchTutorials(getState(), slug)) {
+    return dispatch(fetchTopics(slug));
+  }
 };
