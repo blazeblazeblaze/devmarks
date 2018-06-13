f63a2d385e7ed185e36945099788be9cdc80b739
Blaze Hadzik <hadzikblazej@gmail.com>
2018-06-13 12:45:30 +1000

    Tidy up filters action file.

--- src/actions/filters.js
+++ src/actions/filters.js
@@ -1,8 +1,6 @@
 import * as types from "../constants/ActionTypes";
 
-export const filterTopics = activeFilters => {
-  return {
-    type: types.FILTER_TOPICS,
-    activeFilters
-  };
-};
+export const filterTopics = activeFilters => ({
+  type: types.FILTER_TOPICS,
+  activeFilters
+});
