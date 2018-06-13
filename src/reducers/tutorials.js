185507f792c00a974743fedd58b8c55a7842c422
Blaze Hadzik <hadzikblazej@gmail.com>
2018-06-13 14:41:37 +1000

    Add missing tutorials reducer.

--- src/reducers/tutorials.js
+++ src/reducers/tutorials.js
@@ -1,7 +1,58 @@
-const tutorials = (state = [], action) => {
+import * as types from "../constants/ActionTypes";
+
+const tutorial = (state = {}, action) => {
   switch (action.type) {
-    case "FETCH_TUTORIALS_SUCCESS":
-      return action.tutorials;
+    case types.FETCH_TUTORIALS_REQUESTED:
+      return {
+        ...state,
+        isLoading: true,
+        hasErrors: false,
+        didInvalidate: false,
+        entities: []
+      };
+    case types.FETCH_TUTORIALS_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        hasErrors: false,
+        didInvalidate: false,
+        entities: action.tutorials
+      };
+    case types.FETCH_TUTORIALS_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        hasErrors: true,
+        didInvalidate: false,
+        entities: []
+      };
+    case types.INVALIDATE_TUTORIALS:
+      return {
+        ...state,
+        hasErrors: false,
+        isFetching: false,
+        didInvalidate: true
+      };
+    default:
+      return {
+        isLoading: false,
+        hasErrors: false,
+        didInvalidate: false,
+        entities: []
+      };
+  }
+};
+
+const tutorials = (state = {}, action) => {
+  switch (action.type) {
+    case types.FETCH_TUTORIALS_REQUESTED:
+    case types.FETCH_TUTORIALS_SUCCESS:
+    case types.FETCH_TUTORIALS_ERROR:
+    case types.INVALIDATE_TUTORIALS:
+      return {
+        ...state,
+        [action.slug]: tutorial(state[action.slug], action)
+      };
     default:
       return state;
   }
