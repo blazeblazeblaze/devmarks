fb5c8c44a2e7dc9e946d711b7374f97a7b78b28f
Blaze Hadzik <hadzikblazej@gmail.com>
2018-06-13 12:46:58 +1000

    Standardise dummy api file. Apply 500ms delay to all api calls.

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,43 @@
-import { getTutorials } from "./tutorials";
-import { getTopics, getTopic } from "./topics";
+import { topics, tutorials, topicDetails } from "./raw_data";
+
+const withDelay = (callback, delay = 500) =>
+  setTimeout(() => callback(), delay);
+
+const getTutorials = slug => {
+  return new Promise((resolve, reject) => {
+    const topicTutorials = tutorials[slug];
+
+    if (topicTutorials) {
+      withDelay(() => resolve(topicTutorials));
+    } else {
+      withDelay(() => reject());
+    }
+  });
+};
+
+const getTopicDetails = slug => {
+  return new Promise((resolve, reject) => {
+    const topic = topicDetails[slug];
+    if (topic) {
+      withDelay(() => resolve(topic));
+    } else {
+      withDelay(() => reject());
+    }
+  });
+};
+
+const getTopics = () => {
+  return new Promise((resolve, reject) => {
+    if (topics) {
+      withDelay(() => resolve(topics));
+    } else {
+      withDelay(() => reject());
+    }
+  });
+};
 
 export default {
   getTutorials,
   getTopics,
-  getTopic
+  getTopicDetails
 };
